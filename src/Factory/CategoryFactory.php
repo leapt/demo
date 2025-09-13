6c926d4a7f5d6b62554b1cf75141a34e0c1b5675
jmsche <contact+github@jmsche.fr>
2025-09-13 14:07:14 +0200

    Upgrade Symfony to 7.3.3 & other vendors

--- src/Factory/CategoryFactory.php
+++ src/Factory/CategoryFactory.php
@@ -5,29 +5,10 @@ declare(strict_types=1);
 namespace App\Factory;
 
 use App\Entity\Category;
-use Doctrine\ORM\EntityRepository;
 use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
-use Zenstruck\Foundry\Persistence\Proxy;
-use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
 
 /**
  * @extends PersistentProxyObjectFactory<Category>
- *
- * @method        Category|Proxy                            create(array|callable $attributes = [])
- * @method static Category|Proxy                            createOne(array $attributes = [])
- * @method static Category|Proxy                            find(object|array|mixed $criteria)
- * @method static Category|Proxy                            findOrCreate(array $attributes)
- * @method static Category|Proxy                            first(string $sortedField = 'id')
- * @method static Category|Proxy                            last(string $sortedField = 'id')
- * @method static Category|Proxy                            random(array $attributes = [])
- * @method static Category|Proxy                            randomOrCreate(array $attributes = [])
- * @method static EntityRepository|ProxyRepositoryDecorator repository()
- * @method static Category[]|Proxy[]                        all()
- * @method static Category[]|Proxy[]                        createMany(int $number, array|callable $attributes = [])
- * @method static Category[]|Proxy[]                        createSequence(iterable|callable $sequence)
- * @method static Category[]|Proxy[]                        findBy(array $attributes)
- * @method static Category[]|Proxy[]                        randomRange(int $min, int $max, array $attributes = [])
- * @method static Category[]|Proxy[]                        randomSet(int $number, array $attributes = [])
  */
 final class CategoryFactory extends PersistentProxyObjectFactory
 {
@@ -36,7 +17,10 @@ final class CategoryFactory extends PersistentProxyObjectFactory
         return Category::class;
     }
 
-    protected function defaults(): array|callable
+    /**
+     * @return array{name: string}
+     */
+    protected function defaults(): array
     {
         return [
             'name' => self::faker()->text(),
--- src/Factory/NewsFactory.php
+++ src/Factory/NewsFactory.php
@@ -4,31 +4,14 @@ declare(strict_types=1);
 
 namespace App\Factory;
 
+use App\Entity\Category;
 use App\Entity\Enums\Status;
 use App\Entity\News;
-use Doctrine\ORM\EntityRepository;
 use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
 use Zenstruck\Foundry\Persistence\Proxy;
-use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
 
 /**
  * @extends PersistentProxyObjectFactory<News>
- *
- * @method        News|Proxy                                create(array|callable $attributes = [])
- * @method static News|Proxy                                createOne(array $attributes = [])
- * @method static News|Proxy                                find(object|array|mixed $criteria)
- * @method static News|Proxy                                findOrCreate(array $attributes)
- * @method static News|Proxy                                first(string $sortedField = 'id')
- * @method static News|Proxy                                last(string $sortedField = 'id')
- * @method static News|Proxy                                random(array $attributes = [])
- * @method static News|Proxy                                randomOrCreate(array $attributes = [])
- * @method static EntityRepository|ProxyRepositoryDecorator repository()
- * @method static News[]|Proxy[]                            all()
- * @method static News[]|Proxy[]                            createMany(int $number, array|callable $attributes = [])
- * @method static News[]|Proxy[]                            createSequence(iterable|callable $sequence)
- * @method static News[]|Proxy[]                            findBy(array $attributes)
- * @method static News[]|Proxy[]                            randomRange(int $min, int $max, array $attributes = [])
- * @method static News[]|Proxy[]                            randomSet(int $number, array $attributes = [])
  */
 final class NewsFactory extends PersistentProxyObjectFactory
 {
@@ -37,8 +20,28 @@ final class NewsFactory extends PersistentProxyObjectFactory
         return News::class;
     }
 
-    protected function defaults(): array|callable
+    /**
+     * @return array{
+     *     title: string,
+     *     slug: string,
+     *     content: string,
+     *     publicationDate: \DateTime,
+     *     authorName: string,
+     *     authorEmail: string,
+     *     category: Category&Proxy<Category>,
+     *     image: string|null,
+     *     status: Status,
+     *     active: bool,
+     * }
+     */
+    protected function defaults(): array
     {
+        $image = self::faker()->optional(.7)->passthrough('static-files/' . self::faker()->numberBetween(1, 10) . '.jpg');
+        \assert(null === $image || \is_string($image));
+
+        $status = self::faker()->randomElement(Status::cases());
+        \assert($status instanceof Status);
+
         return [
             'title' => self::faker()->word(),
             'slug' => self::faker()->slug(),
@@ -47,19 +50,9 @@ final class NewsFactory extends PersistentProxyObjectFactory
             'authorName' => self::faker()->userName(),
             'authorEmail' => self::faker()->email(),
             'category' => CategoryFactory::random(),
-            'image' => self::faker()->optional(.7)->passthrough('static-files/' . self::faker()->numberBetween(1, 10) . '.jpg'),
-            'status' => self::faker()->randomElement(Status::cases()),
+            'image' => $image,
+            'status' => $status,
             'active' => self::faker()->boolean(),
         ];
-    }
-
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-     */
-    protected function initialize(): static
-    {
-        return $this
-            // ->afterInstantiate(function(News $news): void {})
-        ;
     }
 }
